45681efcd8ffdc2db7208e4fe6410d039048e453
Tushar Gosavi <Tushar.Gosavi@cognizant.com>
2025-04-09 17:25:20 +0530

    Added login and register pages. Need to remove Security dependency.

--- src/main/java/com/project/InsuranceManagementSystem/Controller/CustomerController.java
+++ src/main/java/com/project/InsuranceManagementSystem/Controller/CustomerController.java
@@ -1,6 +1,20 @@
 package com.project.InsuranceManagementSystem.Controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
 @Controller
 public class CustomerController {
 
+    @RequestMapping("home")
+    public String showLoginPage() {
+        return "auth/login"; // Returns the login page view
+    }
+
+    @RequestMapping("register")
+    public String showRegisterPage() {
+        return "auth/register"; // Returns the register page view
+    }
 }
